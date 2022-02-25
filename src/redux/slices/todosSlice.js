98bb65b6089587416c865815cc598d2295b64ac9
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-25 20:45:31 +0700

    [Feat] redux async thunk todos services

--- src/redux/slices/todosSlice.js
+++ src/redux/slices/todosSlice.js
@@ -1,48 +1,101 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import dayjs from 'dayjs';
+import { todoService } from "../../services/todoService";
+
+// Redux thunk async
+export const fetchTodos = createAsyncThunk(
+  'todos/fetchTodos',
+  async (id) => {
+    const todos = await todoService.getTodos(id);
+
+    return todos;
+  }
+);
+
+// export const fetchTodosByDate = createAsyncThunk(
+//   'todos/fetchTodosByDate',
+//   async (date, thunkApi) => {
+//     const todos = await todoService.getTodosByDate(date);
+//     todos.map(todo => {
+//       thunkApi.dispatch(addTodo(todo));
+//     });
+//     // return { date, todos };
+//   }
+// );
+
+export const fetchAddTodo = createAsyncThunk(
+  'todos/fetchAddTodo',
+  async (data, { dispatch }) => {
+    data.completed = false;
+    data.date = data.date ? dayjs(data.date).format('YYYY-MM-DD') : dayjs().format('YYYY-MM-DD');
+    data.createdAt = new Date();
+
+    try {
+      dispatch(addTodo(data));
+      const res = await todoService.addTodo(data);
+    } catch (e) {
+      console.log(err);
+    }
+  }
+);
+
+export const fetchUpdateTodo = createAsyncThunk(
+  'todos/fetchUpdateTodo',
+  async (data, { dispatch }) => {
+    const { id, todo } = data;
+    dispatch(updateTodo(data));
+
+    await todoService.updateTodo(id, todo);
+  }
+);
+
+export const fetchRemoveTodo = createAsyncThunk(
+  'todos/fetchRemoveTodo',
+  async (id, thunkApi) => {
+    thunkApi.dispatch(removeTodo({ id }));
+    const res = await todoService.removeTodo(id);
+  }
+);
 
 const todosSlice = createSlice({
   name: "todos",
-  initialState: [
-    {
-      id: 0,
-      time: 1645614303888,
-      task: "Cooking a tuna fish",
-      description: "Cooking is my favorite activity",
-      completed: false,
-    }
-  ],
+  initialState: [],
   reducers: {
+    replaceState(state, { payload }) {
+      return payload;
+    },
     addTodo(state, action) {
-      const { task, description, time } = action.payload;
-
-      console.log({
-        task, description, time
-      });
+      const { task, description, date, createdAt } = action.payload;
 
       state.push({
         id: state.length + 1,
         task,
         description,
+        date,
+        createdAt,
         completed: false,
-        time: time || new Date().getTime()
       });
-
-      console.log(state);
     },
     updateTodo(state, action) {
       const { id, todo } = action.payload;
+      console.log(action.payload);
       const index = state.findIndex(item => item.id == id);
+      console.log({ ...state[index], ...todo });
       state[index] = { ...state[index], ...todo };
     },
     removeTodo(state, action) {
       const { id } = action.payload;
       return state.filter(item => item.id != id);
     }
+  },
+  extraReducers: builder => {
+    builder.addCase(fetchTodos.fulfilled, (state, action) => {
+      return action.payload;
+    });
   }
 });
 
-export const { addTodo, updateTodo, removeTodo } = todosSlice.actions;
+export const { addTodo, updateTodo, removeTodo, replaceState } = todosSlice.actions;
 
 export const selector = {
   getTodos: (date) => {
@@ -53,8 +106,7 @@ export const selector = {
     return (state) => {
       const dt = dayjs(date).format('YYYY-MM-DD');
       const todos = state.todos.filter(item => {
-        const formattedDate = dayjs(item.time).format('YYYY-MM-DD');
-        return dt == formattedDate;
+        return dt == item.date;
       });
 
       return todos;
