e57d89e785341cf08a277aca7471637d75c7869a
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-25 20:57:11 +0700

    [Improve] Copy write

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function LoadingScreen() {
   return (
     <div className="h-[100vh] flex justify-center items-center">
-      <img src="/icon.svg" alt="Todo Daily" />
+      <div className='text-center'>
+        <img src="/icon.svg" className='mx-auto mb-4' alt="Todo Daily" />
+        <span>
+          Initializing
+        </span>
+      </div>
     </div>
   );
 }
