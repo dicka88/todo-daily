a19ffa54871c16f821191d82f7ed77348bcbf3b9
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-25 20:55:21 +0700

    [Fix] remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import store, { persistor } from './redux/store';
+import App from './App';
+import LoadingScreen from './components/LoadingScreen';
 
 // CSS
 import "./styles/index.css";
-import LoadingScreen from './components/LoadingScreen';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <PersistGate loading={<LoadingScreen />} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
