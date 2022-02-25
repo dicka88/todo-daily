2b5a5b6888db164f207350fa0134ffb875ffe017
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-25 20:44:37 +0700

    [Feat] environment variable

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+export const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+export const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+export const FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+export const FIREBASE_DATABASE_URL = import.meta.env.VITE_FIREBASE_DATABASE_URL;
+export const FIREBASE_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
