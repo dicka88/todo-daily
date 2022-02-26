418b88a02001bdb7bbefdb6b69830908db288253
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-26 11:02:14 +0700

    fix(outsideclickhandler): use-onclickoutside

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { BsFillTrashFill } from 'react-icons/bs';
 import { BsFillPencilFill, BsThreeDots, BsArchive } from 'react-icons/bs';
-import OutsideClickHandler from 'react-outside-click-handler';
 import { useDispatch } from 'react-redux';
+import useOnClickOutside from 'use-onclickoutside';
 import { fetchRemoveTodo, fetchUpdateTodo } from '../redux/slices/todosSlice';
 import TaskField from './TaskField';
 
@@ -15,8 +15,9 @@ const TodoList = ({
   const dispatch = useDispatch();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [editState, setEditState] = useState(false);
+  const dropdownRef = useRef();
 
-  const handleDropdownFocus = () => {
+  const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
@@ -33,6 +34,8 @@ const TodoList = ({
     dispatch(fetchRemoveTodo(id));
   };
 
+  useOnClickOutside(dropdownRef, () => dropdownOpen && setDropdownOpen(false));
+
   if (editState) return (
     <TaskField
       id={id}
@@ -42,6 +45,7 @@ const TodoList = ({
       onCancel={() => setEditState(false)}
     />
   );
+
   return (
     <div className='flex mb-2'>
       <div className="pr-2">
@@ -63,30 +67,26 @@ const TodoList = ({
           <h1 className="font-bold">{task}</h1>
           <p className="text-gray">{description}</p>
         </div>
-        <div className={`flex items-center text-gray transition-all duration-200 ${dropdownOpen ? '' : 'opacity-0'} group-hover:opacity-100`}>
+        <div ref={dropdownRef} className={`flex items-center text-gray transition-all duration-200 ${dropdownOpen ? '' : 'opacity-0'} group-hover:opacity-100`}>
           <button className='rounded-full p-2 transition-colors duration-300 hover:bg-graySoft' onClick={() => setEditState(true)}>
             <BsFillPencilFill />
           </button>
-          <button className="rounded-full p-2 transition-colors duration-300 hover:bg-graySoft" onClick={handleDropdownFocus}>
+          <button className="rounded-full p-2 transition-colors duration-300 hover:bg-graySoft" onClick={toggleDropdown}>
             <BsThreeDots />
           </button>
           <div className="relative">
-            <OutsideClickHandler
-              onOutsideClick={() => dropdownOpen && handleDropdownFocus()}
-            >
-              <div className={`absolute z-10 top-4 rounded-md right-0 border border-graySoft transition-all duration-400 bg-white ${dropdownOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}>
-                <div className='px-4 hover:bg-graySoft cursor-pointer py-2 flex items-center'>
-                  <BsArchive size={16} className="mr-4 inline" />
-                  <span>Archive</span>
-                </div>
-                <button className="w-full" onClick={() => handleRemove(id)}>
-                  <div className='text-primary px-4 hover:bg-graySoft cursor-pointer py-2 flex items-center'>
-                    <BsFillTrashFill size={16} className="mr-4 inline" />
-                    <span>Delete</span>
-                  </div>
-                </button>
+            <div className={`absolute z-10 top-4 rounded-md right-0 border border-graySoft transition-all duration-400 bg-white ${dropdownOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+              <div className='px-4 hover:bg-graySoft cursor-pointer py-2 flex items-center'>
+                <BsArchive size={16} className="mr-4 inline" />
+                <span>Archive</span>
               </div>
-            </OutsideClickHandler>
+              <button className="w-full" onClick={() => handleRemove(id)}>
+                <div className='text-primary px-4 hover:bg-graySoft cursor-pointer py-2 flex items-center'>
+                  <BsFillTrashFill size={16} className="mr-4 inline" />
+                  <span>Delete</span>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
