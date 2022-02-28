8807a70b479cf185fa0f8be4dba7a65a568d7017
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-28 19:00:29 +0700

    Update(input)

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -7,7 +7,7 @@ export default function Input({
   return (
     <input
       type="text"
-      className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+      className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-graySoft rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
       {...props}
     />
   );
