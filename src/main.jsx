7a783fb003a4a5bb1e898ebbed9179783d19273d
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-04 11:27:42 +0700

    Refactor: remove unused react fragment

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import store, { persistor } from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import store, { persistor } from "./redux/store";
 
-import App from './App';
-import LoadingScreen from './components/LoadingScreen';
+import App from "./App";
+import LoadingScreen from "./components/LoadingScreen";
 
 // CSS
 import "./styles/index.css";
 
 ReactDOM.render(
-  <>
-    <Provider store={store}>
-      <PersistGate loading={<LoadingScreen />} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
