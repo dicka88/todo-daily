5033307aa1e27c6619b4de2d8b0628d057e8db7a
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-04 11:27:21 +0700

    Refactor(auth): change auth route

--- src/components/auth/AuthRoute.jsx
+++ src/components/auth/AuthRoute.jsx
@@ -1,13 +1,13 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { selectUser } from '../../redux/slices/authSlice';
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, Route } from "react-router-dom";
+import { selectUser } from "../../redux/slices/authSlice";
 
-export default function AuthRoute({ children }) {
+export default function AuthRoute() {
   const user = useSelector(selectUser);
 
   if (!user.isLogged) {
     return <Navigate to="/signin" replace />;
   }
 
-  return children;
+  return <Outlet />;
 }
