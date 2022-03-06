016b3acd351d931fbaa81bcb9fc500acd70630d1
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-06 16:08:32 +0700

    Feat(page): 404 not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center">
+        <img
+          src="/notfound.png"
+          className="max-w-[120px] mx-auto mb-4"
+          alt=""
+        />
+        <span className="text-primary text-5xl block font-bold my-10">
+          Oops..
+        </span>
+        <span className="text-lg block text-gray">Page seem is not found</span>
+        <Link to="/" className="text-primary">
+          Go back
+        </Link>
+      </div>
+    </div>
   );
 }
