e2da303b0e8c87c28ebb575d657458cc306a1d8a
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-11 15:52:17 +0700

    Implementation PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    workbox: {
+      cleanupOutdatedCaches: true
+    },
+    manifest: {
+      name: "Todo Daily",
+      short_name: "Todo",
+      description: "Note your task, productive all day",
+      theme_color: "#FF4F5A",
+      icons: [
+        {
+          src: '/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
